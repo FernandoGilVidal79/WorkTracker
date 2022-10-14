4f11dd54bd256036ba2f030af5cf455bc525e640
Fernando Gil <fgil@travelloop.es>
2022-10-14 18:51:43 +0200

    Añadiendo Swagger y primer controlador con User Mock

---
+++ src/WorkTrackerAPI/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkTrackerAPI.Model;
+
+namespace WorkTrackerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            var listUser = new List<User>();
+            listUser.Add(new Model.User()
+            {
+                IdUser = 1,
+                Name = "fernando"
+            });
+
+            return listUser;
+        }
+    }
+}
---
+++ src/WorkTrackerAPI/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkTrackerAPI.Model
+{
+    public class User
+
+    {
+
+        public int IdUser { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
+}
