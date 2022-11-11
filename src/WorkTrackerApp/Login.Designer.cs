3668a45aab151344a5678e852fa25d1bfc3ee38c
SPRaistlin <mariano85sp@gmail.com>
2022-11-11 16:37:03 +0100

    Add changes to template forms and solving conflicts

--- src/WorkTrackerApp/Login.Designer.cs
+++ src/WorkTrackerApp/Login.Designer.cs
@@ -34,117 +34,80 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtContrasena = new System.Windows.Forms.TextBox();
             this.btnIniciarSesion = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pcBoxLogo = new System.Windows.Forms.PictureBox();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.lgnStripResult = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcBoxLogo)).BeginInit();
             this.lgnStripResult.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-<<<<<<< HEAD
             this.label1.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(406, 288);
+            this.label1.Location = new System.Drawing.Point(609, 405);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 20);
-=======
-            this.label1.Location = new System.Drawing.Point(281, 259);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
->>>>>>> d291113f73f3b5b50a682a87903954436778b882
             this.label1.TabIndex = 1;
             this.label1.Text = "E-mail";
             // 
             // txtUsuario
             // 
-<<<<<<< HEAD
             this.txtUsuario.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtUsuario.ForeColor = System.Drawing.Color.DimGray;
-            this.txtUsuario.Location = new System.Drawing.Point(293, 326);
-            this.txtUsuario.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtUsuario.Location = new System.Drawing.Point(440, 464);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(306, 28);
+            this.txtUsuario.Size = new System.Drawing.Size(457, 28);
             this.txtUsuario.TabIndex = 2;
-=======
-            this.textBox1.Location = new System.Drawing.Point(202, 274);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(205, 20);
-            this.textBox1.TabIndex = 2;
->>>>>>> d291113f73f3b5b50a682a87903954436778b882
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-<<<<<<< HEAD
             this.label2.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(395, 398);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(592, 574);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(83, 20);
-=======
-            this.label2.Location = new System.Drawing.Point(270, 324);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
->>>>>>> d291113f73f3b5b50a682a87903954436778b882
             this.label2.TabIndex = 3;
             this.label2.Text = "Contraseña";
             // 
             // txtContrasena
             // 
-<<<<<<< HEAD
             this.txtContrasena.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtContrasena.ForeColor = System.Drawing.Color.DimGray;
-            this.txtContrasena.Location = new System.Drawing.Point(293, 434);
-            this.txtContrasena.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtContrasena.Location = new System.Drawing.Point(440, 630);
+            this.txtContrasena.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.txtContrasena.Name = "txtContrasena";
             this.txtContrasena.PasswordChar = '*';
-            this.txtContrasena.Size = new System.Drawing.Size(306, 28);
+            this.txtContrasena.Size = new System.Drawing.Size(457, 28);
             this.txtContrasena.TabIndex = 4;
-=======
-            this.textBox2.Location = new System.Drawing.Point(202, 341);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(205, 20);
-            this.textBox2.TabIndex = 4;
->>>>>>> d291113f73f3b5b50a682a87903954436778b882
             // 
             // btnIniciarSesion
             // 
-<<<<<<< HEAD
             this.btnIniciarSesion.BackColor = System.Drawing.Color.Silver;
             this.btnIniciarSesion.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnIniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnIniciarSesion.Font = new System.Drawing.Font("Tempus Sans ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIniciarSesion.Location = new System.Drawing.Point(293, 520);
-            this.btnIniciarSesion.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnIniciarSesion.Location = new System.Drawing.Point(440, 762);
+            this.btnIniciarSesion.Margin = new System.Windows.Forms.Padding(6, 8, 6, 8);
             this.btnIniciarSesion.Name = "btnIniciarSesion";
-            this.btnIniciarSesion.Size = new System.Drawing.Size(306, 49);
+            this.btnIniciarSesion.Size = new System.Drawing.Size(459, 75);
             this.btnIniciarSesion.TabIndex = 5;
             this.btnIniciarSesion.Text = "Iniciar Sesión";
             this.btnIniciarSesion.UseVisualStyleBackColor = false;
-=======
-            this.button1.Location = new System.Drawing.Point(202, 391);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(205, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Iniciar Sesión";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
->>>>>>> d291113f73f3b5b50a682a87903954436778b882
             // 
-            // pictureBox1
+            // pcBoxLogo
             // 
-            this.pictureBox1.Image = global::WorkTrackerAPP.Properties.Resources.Logo;
-            this.pictureBox1.Location = new System.Drawing.Point(202, 8);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pcBoxLogo.Image = global::WorkTrackerAPP.Properties.Resources.Logo;
+            this.pcBoxLogo.Location = new System.Drawing.Point(503, 12);
+            this.pcBoxLogo.Name = "pcBoxLogo";
+            this.pcBoxLogo.Size = new System.Drawing.Size(322, 316);
+            this.pcBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcBoxLogo.TabIndex = 0;
+            this.pcBoxLogo.TabStop = false;
             // 
             // lgnStripResult
             // 
@@ -155,10 +118,12 @@
             this.lgnStripResult.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
             this.lgnStripResult.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
-            this.lgnStripResult.Location = new System.Drawing.Point(0, 660);
+            this.lgnStripResult.Location = new System.Drawing.Point(0, 1029);
             this.lgnStripResult.Name = "lgnStripResult";
-            this.lgnStripResult.Size = new System.Drawing.Size(968, 25);
+            this.lgnStripResult.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
+            this.lgnStripResult.Size = new System.Drawing.Size(1452, 25);
             this.lgnStripResult.TabIndex = 38;
+            this.lgnStripResult.Visible = false;
             // 
             // toolStripStatusLabel1
             // 
@@ -168,33 +133,24 @@
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< HEAD
-            this.ClientSize = new System.Drawing.Size(968, 685);
+            this.ClientSize = new System.Drawing.Size(1452, 1054);
             this.Controls.Add(this.lgnStripResult);
             this.Controls.Add(this.btnIniciarSesion);
             this.Controls.Add(this.txtContrasena);
-=======
-            this.ClientSize = new System.Drawing.Size(645, 445);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
->>>>>>> d291113f73f3b5b50a682a87903954436778b882
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtUsuario);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
-<<<<<<< HEAD
+            this.Controls.Add(this.pcBoxLogo);
             this.ForeColor = System.Drawing.Color.DarkRed;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-=======
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
->>>>>>> d291113f73f3b5b50a682a87903954436778b882
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Login";
             this.Text = "Work Tracker";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcBoxLogo)).EndInit();
             this.lgnStripResult.ResumeLayout(false);
             this.lgnStripResult.PerformLayout();
             this.ResumeLayout(false);
@@ -204,7 +160,7 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pcBoxLogo;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtUsuario;
         private System.Windows.Forms.Label label2;
