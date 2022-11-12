010670568e940ee487c2558b18b2222c0a7a9b80
SPRaistlin <mariano85sp@gmail.com>
2022-11-12 13:00:13 +0100

    Add code to login width real user

--- src/WorkTrackerApp/Login.cs
+++ src/WorkTrackerApp/Login.cs
@@ -19,34 +19,68 @@ namespace WorkTrackerAPP
             InitializeComponent();
             
         }
-        public static Boolean ValidarCampos(String nombreUsuario, String contrasena)
+        public static Boolean ValidarCampos(TextBox nombreUsuario, TextBox contrasena)
         {
-            if (nombreUsuario != "" || contrasena != "")
-                return true;
-            else 
+            if (nombreUsuario.Text == "")
+            {
+                nombreUsuario.Focus();
+                MensajeError("El Nombre de usuario está vacío", "Error");
                 return false;
+
+            }
+            if (contrasena.Text == "")
+            {
+                contrasena.Focus();
+                MensajeError("La Contraseña no es correcta", "Error");
+                return false;
+            }
+                return true;
+
         }
+        /*
+         * Mensaje de error 
+         */
         public static void MensajeError(String mensaje, String cabecera)
         {
             MessageBox.Show(mensaje, cabecera, MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
+        /*
+         * Mensaje de OK
+         */
         public static void MensajeOk(String mensaje, String cabecera)
         {
             MessageBox.Show(mensaje, cabecera, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
         private void btnIniciarSesion_Click(object sender, EventArgs e)
         {
-            var validar = ValidarCampos(txtUsuario.Text, txtContrasena.Text);
+            //Validamos los campos
+            var validar = ValidarCampos(txtUsuario, txtContrasena);
+            //En caso de que la validación sea correcta obtenemos el usuario
             if(validar)
             {
+                //Llamamos a al api para obtener el usuario
                 var apiclient = new UserApi("http://worktracker-001-site1.atempurl.com/");
-                var users = apiclient.ApiUserLoginGetWithHttpInfo("Mariano", "Mariano");
+                var users = apiclient.ApiUserLoginGetWithHttpInfo(txtUsuario.Text, txtContrasena.Text);
+
+                //Comprobamos que el usuario existe, en caso de existir entra en al APP, en caso contrario muestra un mensaje
                 var user = users.Data;
-                Console.WriteLine(user.Email);
-                MensajeOk("Bienvenido" + user.UserName, "Correcto");
+                if(user != null)
+                {
+                    Console.WriteLine(user);
+                    MensajeOk("Bienvenido " + user.UserName, "Correcto");
+                    this.Hide();
+                    MenuPrincipal frmMenuPrincipal  = new MenuPrincipal();
+                    frmMenuPrincipal.ShowDialog();
+                    this.Close();
+
+                } else
+                {
+                    MensajeError("El usuario NO existe", "Error");
+                }
+
             } else
             {
-                MensajeError("El usuario NO existe", "Error");
+                MensajeError("Ocurrió un error en la validación", "Error");
             }
 
         }
