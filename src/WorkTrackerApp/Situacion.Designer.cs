64808d95ee5a256671cda422d1f6fc2b29fd6750
sara rubi escacena <sara_rubi@hotmail.com>
2022-11-17 00:30:35 +0100

    Creado Formulario Situacion

---
+++ src/WorkTrackerApp/Situacion.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace WorkTrackerAPP
+{
+    partial class Situacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbAño = new System.Windows.Forms.ComboBox();
+            this.labAño = new System.Windows.Forms.Label();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblSolicitados = new System.Windows.Forms.Label();
+            this.lblPendientesAprobar = new System.Windows.Forms.Label();
+            this.lblAprobados = new System.Windows.Forms.Label();
+            this.txbSolicitados = new System.Windows.Forms.TextBox();
+            this.txbParaAprobar = new System.Windows.Forms.TextBox();
+            this.txbAprobados = new System.Windows.Forms.TextBox();
+            this.lblTipoAusencia = new System.Windows.Forms.Label();
+            this.txbTipoAusencia = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmbAño
+            // 
+            this.cmbAño.Font = new System.Drawing.Font("Tempus Sans ITC", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbAño.FormattingEnabled = true;
+            this.cmbAño.Location = new System.Drawing.Point(551, 55);
+            this.cmbAño.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbAño.Name = "cmbAño";
+            this.cmbAño.Size = new System.Drawing.Size(71, 25);
+            this.cmbAño.TabIndex = 82;
+            // 
+            // labAño
+            // 
+            this.labAño.AutoSize = true;
+            this.labAño.Font = new System.Drawing.Font("Tempus Sans ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labAño.Location = new System.Drawing.Point(484, 55);
+            this.labAño.Name = "labAño";
+            this.labAño.Size = new System.Drawing.Size(43, 22);
+            this.labAño.TabIndex = 83;
+            this.labAño.Text = "Año";
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.AutoSize = true;
+            this.lblSaldo.Font = new System.Drawing.Font("Tempus Sans ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldo.Location = new System.Drawing.Point(274, 92);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(253, 22);
+            this.lblSaldo.TabIndex = 87;
+            this.lblSaldo.Text = "Vacaciones pendientes de disfrutar";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(551, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(71, 22);
+            this.textBox1.TabIndex = 88;
+            // 
+            // lblSolicitados
+            // 
+            this.lblSolicitados.AutoSize = true;
+            this.lblSolicitados.Font = new System.Drawing.Font("Tempus Sans ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSolicitados.Location = new System.Drawing.Point(291, 164);
+            this.lblSolicitados.Name = "lblSolicitados";
+            this.lblSolicitados.Size = new System.Drawing.Size(115, 22);
+            this.lblSolicitados.TabIndex = 89;
+            this.lblSolicitados.Text = "Días solicitados";
+            // 
+            // lblPendientesAprobar
+            // 
+            this.lblPendientesAprobar.AutoSize = true;
+            this.lblPendientesAprobar.Font = new System.Drawing.Font("Tempus Sans ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPendientesAprobar.Location = new System.Drawing.Point(408, 164);
+            this.lblPendientesAprobar.Name = "lblPendientesAprobar";
+            this.lblPendientesAprobar.Size = new System.Drawing.Size(129, 22);
+            this.lblPendientesAprobar.TabIndex = 90;
+            this.lblPendientesAprobar.Text = "Días para aprobar";
+            // 
+            // lblAprobados
+            // 
+            this.lblAprobados.AutoSize = true;
+            this.lblAprobados.Font = new System.Drawing.Font("Tempus Sans ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAprobados.Location = new System.Drawing.Point(539, 164);
+            this.lblAprobados.Name = "lblAprobados";
+            this.lblAprobados.Size = new System.Drawing.Size(115, 22);
+            this.lblAprobados.TabIndex = 91;
+            this.lblAprobados.Text = "Días aprobados";
+            // 
+            // txbSolicitados
+            // 
+            this.txbSolicitados.Location = new System.Drawing.Point(295, 200);
+            this.txbSolicitados.Name = "txbSolicitados";
+            this.txbSolicitados.Size = new System.Drawing.Size(111, 22);
+            this.txbSolicitados.TabIndex = 92;
+            // 
+            // txbParaAprobar
+            // 
+            this.txbParaAprobar.Location = new System.Drawing.Point(412, 200);
+            this.txbParaAprobar.Name = "txbParaAprobar";
+            this.txbParaAprobar.Size = new System.Drawing.Size(125, 22);
+            this.txbParaAprobar.TabIndex = 93;
+            // 
+            // txbAprobados
+            // 
+            this.txbAprobados.Location = new System.Drawing.Point(543, 200);
+            this.txbAprobados.Name = "txbAprobados";
+            this.txbAprobados.Size = new System.Drawing.Size(111, 22);
+            this.txbAprobados.TabIndex = 94;
+            // 
+            // lblTipoAusencia
+            // 
+            this.lblTipoAusencia.AutoSize = true;
+            this.lblTipoAusencia.Font = new System.Drawing.Font("Tempus Sans ITC", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoAusencia.Location = new System.Drawing.Point(47, 164);
+            this.lblTipoAusencia.Name = "lblTipoAusencia";
+            this.lblTipoAusencia.Size = new System.Drawing.Size(111, 22);
+            this.lblTipoAusencia.TabIndex = 95;
+            this.lblTipoAusencia.Text = "Tipo Ausencia";
+            this.lblTipoAusencia.Click += new System.EventHandler(this.lblVacaciones_Click);
+            // 
+            // txbTipoAusencia
+            // 
+            this.txbTipoAusencia.Location = new System.Drawing.Point(51, 200);
+            this.txbTipoAusencia.Name = "txbTipoAusencia";
+            this.txbTipoAusencia.Size = new System.Drawing.Size(226, 22);
+            this.txbTipoAusencia.TabIndex = 96;
+            // 
+            // Situacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txbTipoAusencia);
+            this.Controls.Add(this.lblTipoAusencia);
+            this.Controls.Add(this.txbAprobados);
+            this.Controls.Add(this.txbParaAprobar);
+            this.Controls.Add(this.txbSolicitados);
+            this.Controls.Add(this.lblAprobados);
+            this.Controls.Add(this.lblPendientesAprobar);
+            this.Controls.Add(this.lblSolicitados);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblSaldo);
+            this.Controls.Add(this.labAño);
+            this.Controls.Add(this.cmbAño);
+            this.Name = "Situacion";
+            this.Text = "Situacion";
+            this.Load += new System.EventHandler(this.Situacion_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbAño;
+        private System.Windows.Forms.Label labAño;
+        private System.Windows.Forms.Label lblSaldo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblSolicitados;
+        private System.Windows.Forms.Label lblPendientesAprobar;
+        private System.Windows.Forms.Label lblAprobados;
+        private System.Windows.Forms.TextBox txbSolicitados;
+        private System.Windows.Forms.TextBox txbParaAprobar;
+        private System.Windows.Forms.TextBox txbAprobados;
+        private System.Windows.Forms.Label lblTipoAusencia;
+        private System.Windows.Forms.TextBox txbTipoAusencia;
+    }
+}
