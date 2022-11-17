b0b46f5d37d60217a49e2be52b93215487c49fa1
sara rubi escacena <sara_rubi@hotmail.com>
2022-11-17 23:31:24 +0100

    Carga Forms menu rrhh

--- src/WorkTrackerApp/MenuPrincipalRRHH.cs
+++ src/WorkTrackerApp/MenuPrincipalRRHH.cs
@@ -16,6 +16,15 @@ namespace WorkTrackerAPP
         {
             InitializeComponent();
             TmrHora.Enabled = true;
+            pnlFichar.Controls.Clear();
+            Fichar Frm = new Fichar();
+            Frm.TopLevel = false;
+            Frm.FormBorderStyle = FormBorderStyle.None;
+            Frm.Dock = DockStyle.Fill;
+            pnlFichar.Controls.Add(Frm);
+            pnlFichar.Tag = Frm;
+            Frm.Show();
+            Console.WriteLine(UserSession.User);
             Helper.MensajeBienvenida(lblBienvenida);
         }
         private void TmrHora_Tick(object sender, EventArgs e)
@@ -30,6 +39,55 @@ namespace WorkTrackerAPP
         private void pnlFichar_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void solicitudesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pnlFichar.Controls.Clear();
+            SolicitudAusencia FrmAusencia = new SolicitudAusencia();
+            FrmAusencia.TopLevel = false;
+            FrmAusencia.FormBorderStyle = FormBorderStyle.None;
+            FrmAusencia.Dock = DockStyle.Fill;
+            pnlFichar.Controls.Add(FrmAusencia);
+            pnlFichar.Tag = FrmAusencia;
+            FrmAusencia.Show();
+        }
+
+        private void situaciónToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pnlFichar.Controls.Clear();
+            Situacion FrmSituacion = new Situacion();
+            FrmSituacion.TopLevel = false;
+            FrmSituacion.FormBorderStyle = FormBorderStyle.None;
+            FrmSituacion.Dock = DockStyle.Fill;
+            pnlFichar.Controls.Add(FrmSituacion);
+            pnlFichar.Tag = FrmSituacion;
+            FrmSituacion.Show();
+        }
+
+        private void aprobacionestoolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            pnlFichar.Controls.Clear();
+            AprobacionSolicitudes FrmAprobaciones = new AprobacionSolicitudes();
+            FrmAprobaciones.TopLevel = false;
+            FrmAprobaciones.FormBorderStyle = FormBorderStyle.None;
+            FrmAprobaciones.Dock = DockStyle.Fill;
+            pnlFichar.Controls.Add(FrmAprobaciones);
+            pnlFichar.Tag = FrmAprobaciones;
+            FrmAprobaciones.Show();
+        }
+
+        private void toolStripCreacionUsuario_Click(object sender, EventArgs e)
+        {
+
+            pnlFichar.Controls.Clear(); 
+            CreacionUsuarios FrmCreacion = new CreacionUsuarios();
+            FrmCreacion.TopLevel = false;
+            FrmCreacion.FormBorderStyle = FormBorderStyle.None;
+            FrmCreacion.Dock = DockStyle.Fill;
+            pnlFichar.Controls.Add(FrmCreacion);
+            pnlFichar.Tag = FrmCreacion;
+            FrmCreacion.Show();
         }
     }
 }
