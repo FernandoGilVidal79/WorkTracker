fa5d31a5b22fac4e46cbdc845bcc72db7a41c8c3
Fernando Gil <fgil@travelloop.es>
2022-11-26 18:46:10 +0100

    Añadiendo Controlador Calendar

---
+++ src/WorkTrackerAPI/Model/Calendar.cs
@@ -0,0 +1,15 @@
+﻿using Dapper;
+
+namespace WorkTrackerAPI.Model
+{
+    public class Calendar
+    {
+        [Key]
+        [IgnoreInsert]
+        public int IdCalendar { get; set; } 
+        public int Day {  get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public bool Festive { get; set; }
+    }
+}
